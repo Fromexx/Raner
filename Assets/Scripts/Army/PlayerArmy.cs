a8bd062000820f0a8f5d23b7fa47bf40f0d2d149
MatveyZhizhin <117218670+MatveyZhizhin@users.noreply.github.com>
2023-06-06 17:37:05 +0300

    Revert "Молоток"
    This reverts commit de5f0803c95b0f056fc9a4cfdd1fa6920f435a12.

--- Assets/Scripts/Army/PlayerArmy.cs
+++ Assets/Scripts/Army/PlayerArmy.cs
@@ -4,34 +4,17 @@ using UnityEngine;
 
 public class PlayerArmy : Army
 {
-    [SerializeField] private float speed;
-    private Army army;
-    [SerializeField] private float minDistance;
-    [SerializeField] private float maxDistance;
+    public float speed;
+    Army army;    
 
     private void Start()
     {
-        army = FindAnyObjectByType<EnemyArmy>();
-        OnArmyCountChanged(1);
+        army = GetComponent<EnemyArmy>();
+        SpawnArmy(ArmyCount);
     }
 
     private void Update()
     {
         float distance = Vector3.Distance(army.transform.position, transform.position);
-
-        if (distance < maxDistance)
-        {
-            if (distance > minDistance)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, army.transform.position, speed * Time.deltaTime);
-            }
-        }
-    }
-  
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, maxDistance);
-        Gizmos.DrawWireSphere(transform.position, minDistance);
     }
 }
--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
-<<<<<<< Updated upstream
     [SerializeField] private int _damage;
     [SerializeField] private float _radius;
 
@@ -12,53 +11,10 @@ public class Hammer : MonoBehaviour
         
         foreach (var unit in units)
         {
-=======
-<<<<<<< HEAD
-    [SerializeField] private Transform attackPoint;
-    [SerializeField] private float radius;
-    private PlayerArmy army;
-
-    private void Start()
-    {
-        army = FindObjectOfType<PlayerArmy>();
-    }
-
-    public void Attack()
-    {
-        Collider[] units = Physics.OverlapSphere(attackPoint.position, radius);
-        foreach (Collider unit in units)
-        {
-            if (unit.CompareTag("PlayerUnit"))
-            {
-                army.ArmyCount--;
-                army.units.Remove(unit.gameObject);                
-                Destroy(unit.gameObject);                              
-            }
-=======
-    [SerializeField] private int _damage;
-    [SerializeField] private float _radius;
-
-    public void Attack()
-    {
-        var units = Physics.OverlapSphere(transform.position, _radius);
-        
-        foreach (var unit in units)
-        {
->>>>>>> Stashed changes
             if (!unit.TryGetComponent(out Army army)) continue;
             
             army.OnUnitsDeleting(army.ArmyCount);
             break;
-<<<<<<< Updated upstream
-=======
->>>>>>> 5a5e2b9ac7f348b01d9cc3f0fa3c5d7841f4bf5d
->>>>>>> Stashed changes
         }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
