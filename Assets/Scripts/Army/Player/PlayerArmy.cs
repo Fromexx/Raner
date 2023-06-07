0cd230b7b8c9d536f3b089edaa79a37883aaf3fe
Hitsar <vlex09@ya.ru>
2023-06-07 12:48:34 +0300

    заменил Input на новый, заменил джостик

--- Assets/Scripts/Army/Player/PlayerArmy.cs
+++ Assets/Scripts/Army/Player/PlayerArmy.cs
@@ -4,24 +4,11 @@ namespace Player
 {
     public class PlayerArmy : Army
     {
-        [SerializeField] private FixedJoystick _fixedJoystick;
-
         private PlayerArmyMovement _playerArmyMovement;
 
         private void Awake()
         {
             TryGetComponent(out _playerArmyMovement);
-        }
-
-        private void FixedUpdate()
-        {
-            Movement();
-        }
-
-        private void Movement()
-        {
-            var direction = new Vector3(_fixedJoystick.Direction.x, 0, _fixedJoystick.Direction.y);
-            _playerArmyMovement.Movement(direction);
         }
 
         public void MoveArmyAtPosition(Vector3 targetPosition) => _playerArmyMovement.MoveAtPosition(targetPosition);
--- Assets/Scripts/Army/Player/PlayerArmyMovement.cs
+++ Assets/Scripts/Army/Player/PlayerArmyMovement.cs
@@ -5,9 +5,22 @@ namespace Player
     public class PlayerArmyMovement : MonoBehaviour
     {
         [SerializeField] private float _speed;
-        
-        public void MoveAtPosition(Vector3 targetPosition) => transform.position = Vector3.MoveTowards(transform.position, targetPosition, _speed * Time.deltaTime);
+        private Vector3 _direction;
+        private InputManager _inputManager;
 
-        public void Movement(Vector3 direction) => transform.Translate(direction * _speed, Space.World);
+        private void Awake()
+        {
+            _inputManager = new InputManager();
+            _inputManager.Enable();
+        }
+
+        public void FixedUpdate()
+        {
+            Vector2 receivedVector = _inputManager.Player.Move.ReadValue<Vector2>();
+            _direction = new Vector3(receivedVector.x, 0, receivedVector.y);
+            transform.Translate(_direction * (_speed * Time.fixedDeltaTime), Space.World);
+        }
+
+        public void MoveAtPosition(Vector3 targetPosition) => transform.position = Vector3.MoveTowards(transform.position, targetPosition, _speed * Time.deltaTime);
     }
 }
